13b53c20d66c2577a8d54f06b8cb47b982982583
Surendhar <76396808+surendhar-palanisamy@users.noreply.github.com>
2025-02-06 19:14:48 +0530

    Update modal volume and file paths for batch upload

--- src/modal_functions/volume/push_modal_file.py
+++ src/modal_functions/volume/push_modal_file.py
@@ -1,10 +1,10 @@
 import modal
 from pathlib import Path
 
-volume = modal.Volume.from_name("Visiondata")
+volume = modal.Volume.from_name("NewVisiondata")
 
-file_path = Path("CricaVPR.pth")
+file_path = Path("dino_salad.ckpt")
 
 
 with volume.batch_upload() as batch:
-    batch.put_file(str(file_path), "parameters/CricaVPR/ckpts/CricaVPR.pth")
+    batch.put_file(str(file_path), "parameters/DinoV2Salad/ckpts/dino_salad.ckpt")
