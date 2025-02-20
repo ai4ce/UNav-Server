90cba93917a2e5e4cc7b3b58a3a0482361aa9c33
Surendhar <76396808+surendhar-palanisamy@users.noreply.github.com>
2025-02-20 22:26:22 +0530

    Remove redundant constructor definition in Coarse_Locator class (#35)

--- src/UNav_core/src/track/hierarchical_localization.py
+++ src/UNav_core/src/track/hierarchical_localization.py
@@ -32,7 +32,6 @@ def read_pickle_file(file_path):
 
 class Coarse_Locator:
     def __init__(self, feature, config):
-    def __init__(self, feature, config):
         """
         Initializes the VPR class.
         :param root: Path to the directory containing the combined global_features.h5 file.
