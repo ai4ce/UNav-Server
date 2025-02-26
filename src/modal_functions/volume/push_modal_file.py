ff7a450505389ecbe62268996cc2ffd2b9fa0ca7
Surendhar <76396808+surendhar-palanisamy@users.noreply.github.com>
2025-02-26 14:07:06 +0530

    Update volume file paths and rename folder in modal functions

--- src/modal_functions/volume/push_modal_file.py
+++ src/modal_functions/volume/push_modal_file.py
@@ -1,10 +1,12 @@
 import modal
 from pathlib import Path
 
-volume = modal.Volume.from_name("Visiondata")
+volume = modal.Volume.from_name("NewVisiondata")
 
-file_path = Path("CricaVPR.pth")
+file_path = Path("path.h5")
 
 
 with volume.batch_upload() as batch:
-    batch.put_file(str(file_path), "parameters/CricaVPR/ckpts/CricaVPR.pth")
+    batch.put_file(
+        str(file_path), "data/New_York_City/LightHouse/6_floor/path.h5"
+    )
