3044c414b4c88c12eb49939a6245175e288249fd
Surendhar <76396808+surendhar-palanisamy@users.noreply.github.com>
2025-05-15 21:31:17 +0530

    Fix destination_id in main function to "00856"

--- src/modal_functions/test_modal_functions.py
+++ src/modal_functions/test_modal_functions.py
@@ -11,7 +11,7 @@ def main():
     full_image_path = os.path.join(
         current_directory, "modal_functions/misc/sample_image_4.jpg"
     )
-    destination_id = "01547"
+    destination_id = "00856"
     floor = "6_floor"
     with open(full_image_path, "rb") as image_file:
         image_data = image_file.read()
@@ -19,16 +19,16 @@ def main():
 
     print(unav_server.get_destinations_list.remote(floor=floor))
 
-    print(
-        unav_server.planner.remote(
-            destination_id=destination_id,
-            base_64_image=base64_encoded,
-            session_id="test_session_id_2",
-            building="LightHouse",
-            floor=floor,
-            place="New_York_City",
-        )
-    )
+    # print(
+    #     unav_server.planner.remote(
+    #         destination_id=destination_id,
+    #         base_64_image=base64_encoded,
+    #         session_id="test_session_id_2",
+    #         building="LightHouse",
+    #         floor=floor,
+    #         place="New_York_City",
+    #     )
+    # )
 
 
 if __name__ == "__main__":
