a34ce9e61bbb3c394abf95a397a85a2acd22e0e2
Surendhar <76396808+surendhar-palanisamy@users.noreply.github.com>
2025-09-12 13:10:38 +0530

    Update test.py to use sample_image_20.jpg and adjust destination details for Mahidol University

--- src/modal_functions/unav_v2/test.py
+++ src/modal_functions/unav_v2/test.py
@@ -11,33 +11,33 @@ def main():
         UnavServer = modal.Cls.lookup("unav-server-v21", "UnavServer")
         unav_server = UnavServer()
         current_directory = os.getcwd()
-        full_image_path = os.path.join(
-            current_directory, "sample_image_7.jpg"
-        )
-        destination_id = "42"
-        floor = "3_floor"
+        full_image_path = os.path.join(current_directory, "sample_image_20.jpg")
+        destination_id = "32"
+        floor = ("fl2",)
         with open(full_image_path, "rb") as image_file:
             image_data = image_file.read()
             base64_encoded = base64.b64encode(image_data).decode("utf-8")
 
-
-        print("Testing get_destinations_list...")
-        result = unav_server.get_destinations_list.remote(floor="17_floor")
-        print("Result:", result)
+        # print("Testing get_destinations_list...")
+        # result = unav_server.get_destinations_list.remote(
+        #     floor="fl2",
+        #     place="Mahidol_University",
+        #     building="Jubilee",
+        # )
+        # print("Result:", result)
 
         print(
             unav_server.planner.remote(
                 destination_id=destination_id,
                 base_64_image=base64_encoded,  # Pass BGR numpy array instead of base64
                 session_id="test_session_id_2",
-                building="LightHouse",
+                building="Jubilee",
                 floor=floor,
-                place="New_York_City",
+                place="Mahidol_University",
             )
         )
     except Exception as e:
         print(f"Error during Modal class lookup or execution: {e}")
-
 
 
 if __name__ == "__main__":
